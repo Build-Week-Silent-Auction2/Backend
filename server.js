df463efab6c51aea9f6835e99a596fbb7ce333a2
Chad.Snyder <Mapletonkid@gmail.com>
2020-02-29 17:48:16 -0700

    dep3

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ server.use("/api", sellersRouter);
 // * sanity
 
 server.get('/', (req,res) =>{
-  res.status(200).json(<h1>Hello World</h1>)
+  res.send(<h1>Hello World</h1>)
 });
 
 module.exports = server;
