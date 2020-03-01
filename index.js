8b1ebd9426eb8060b959c41686298bded10ddf1f
Chad.Snyder <Mapletonkid@gmail.com>
2020-03-01 10:58:59 -0700

    hair pull out3

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const server = require('./server');
 
-const port = process.env.port || 5000;
-
-server.listen(port, () => console.log(' *** Server Listening on port' + port));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+});
